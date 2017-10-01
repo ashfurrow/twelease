dd6e7e64410e148527028b4625ae0daa09a29aa5
Ash Furrow <ash@ashfurrow.com>
2017-10-01 13:17:00 -0400

    Includes repo name.

--- src/eventHandler.ts
+++ src/eventHandler.ts
@@ -1,10 +1,10 @@
 import * as Mustache from 'mustache'
 import tweet from './twitter'
 
-const handleNewTag = (tagName: string, pusherName: string): void => {
+const handleNewTag = (tagName: string, pusherName: string, repoURL: string): void => {
   console.log(`${pusherName} pushed new tag ${tagName}`)
   const template = process.env['TWEET_MUSTACHE_TEMPLATE']
-  const tweetText = Mustache.render(template, {tagName, pusherName})
+  const tweetText = Mustache.render(template, {tagName, pusherName, repoURL})
   tweet(tweetText)
 }
 
@@ -13,7 +13,8 @@ const eventHandler = (event, repoName, data) => {
   if (event === 'create' && data.ref_type === 'tag') {
     const tagName: string = data.ref
     const pusherName: string = data.sender.login
-    handleNewTag(tagName, pusherName)
+    const repoURL: string = `${data.repository.html_url}`
+    handleNewTag(tagName, pusherName, repoURL)
   }
 }
 
